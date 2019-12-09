7f98388ba2371683f08d4d7452eda661ffd4c4ff
jbaldwin <joshrbaldwin@gmail.com>
2019-12-08 22:38:17 -0700

    2019 day8 p1,p2

---
+++ 2019/day8/p1/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <lib/FileUtil.h>
+
+int main(int argc, char* argv[])
+{
+    std::vector<std::string> args{argv, argv + argc};
+    if(args.size() < 4)
+    {
+        std::cerr << args[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    std::string data = file::read(args[1]);
+
+    std::size_t width = std::stoul(args[2]);
+    std::size_t height = std::stoul(args[3]);
+
+    std::size_t layer_length = width * height;
+
+    std::vector<std::string> layers{};
+    while(data.length() >= layer_length)
+    {
+        auto layer_data = data.substr(0, layer_length);
+        data = data.substr(layer_length);
+
+        layers.emplace_back(std::move(layer_data));
+    }
+    // if(!data.empty())
+    // {
+    //     layers.emplace_back(std::move(data));
+    // }
+
+    std::size_t fewest_zeros = std::numeric_limits<std::size_t>::max();
+    std::size_t fewest_zeros_idx = 0;
+    for(std::size_t i = 0; i < layers.size(); ++i)
+    {
+        const auto& layer = layers[i];
+        std::size_t zeros = 0;
+        for(const auto i : layer)
+        {
+            if(i == '0')
+            {
+                ++zeros;
+            }
+        }
+
+        std::cout << layer << " zeros: " << zeros << std::endl;
+
+        if(zeros < fewest_zeros)
+        {
+            fewest_zeros = zeros;
+            fewest_zeros_idx = i;
+        }
+    }
+
+    std::cout << "idx: " << fewest_zeros_idx << std::endl;
+
+    std::size_t ones = 0;
+    std::size_t twos = 0;
+
+    for(const auto i : layers[fewest_zeros_idx])
+    {
+        if(i == '1')
+        {
+            ++ones;
+        }
+        else if(i == '2')
+        {
+            ++twos;
+        }
+    }
+
+    std::cout << (ones * twos) << std::endl;
+
+    return 0;
+}
---
+++ 2019/day8/p2/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <lib/FileUtil.h>
+
+int main(int argc, char* argv[])
+{
+    std::vector<std::string> args{argv, argv + argc};
+    if(args.size() < 4)
+    {
+        std::cerr << args[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    std::string data = file::read(args[1]);
+
+    std::size_t width = std::stoul(args[2]);
+    std::size_t height = std::stoul(args[3]);
+
+    std::size_t layer_length = width * height;
+
+    std::vector<std::vector<std::string>> layers{};
+
+    while(data.length() >= layer_length)
+    {
+        auto layer_data = data.substr(0, layer_length);
+        data = data.substr(layer_length);
+
+        std::vector<std::string> row_data{};
+        row_data.reserve(height);
+
+        while(layer_data.length() >= width)
+        {
+            auto col_data = layer_data.substr(0, width);
+            layer_data = layer_data.substr(width);
+
+            row_data.emplace_back(std::move(col_data));
+        }
+
+        layers.push_back(std::move(row_data));
+    }
+
+    std::vector<std::string> image{};
+    image.resize(height);
+    for(auto& row : image)
+    {
+        row.resize(width);
+    }
+
+    for(std::size_t r = 0; r < height; ++r)
+    {
+        for(std::size_t c = 0; c < width; ++c)
+        {
+            image[r][c] = '2';
+        }
+    }
+
+    for(const auto& layer : layers)
+    {
+        for(std::size_t r = 0; r < height; ++r)
+        {
+            for(std::size_t c = 0; c < width; ++c)
+            {
+                if(layer[r][c] != '2' && image[r][c] == '2')
+                {
+                    image[r][c] = layer[r][c];
+                }
+            }
+        }
+    }
+
+    for(std::size_t r = 0; r < height; ++r)
+    {
+        for(std::size_t c = 0; c < width; ++c)
+        {
+            if(image[r][c] == '1')
+            {
+                std::cout << '1';
+            }
+            else
+            {
+                std::cout << ' ';
+            }
+            // std::cout << image[r][c];
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
