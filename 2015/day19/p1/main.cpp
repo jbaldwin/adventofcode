e4ce02ba0f2232456e773904bd57d369a73469a2
jbaldwin <joshrbaldwin@gmail.com>
2020-03-01 19:02:22 -0700

    2015 day19 p1
    p2 *should* work but seems to be taking
    forever to complete, compared to other
    solutions and it seems like its good
    with astar but it must have a bug somewhere

---
+++ 2015/day19/p1/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+
+#include <lib/FileUtil.h>
+#include <lib/StringUtil.h>
+
+int main(int argc, char* argv[])
+{
+    std::vector<std::string> args{argv, argv + argc};
+    if(args.size() != 2)
+    {
+        std::cout << args[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    auto contents = file::read(args[1]);
+
+    auto parts = str::split(contents, "\n\n");
+    auto replacement_inputs_all = parts[0];
+    auto medicine_molecule = parts[1];
+
+    std::multimap<std::string, std::string> replacements{};
+    for(const auto& line : str::split(replacement_inputs_all, '\n'))
+    {
+        auto replacement_parts = str::split(line, " => ");
+        replacements.emplace(
+            std::string{replacement_parts[0]},
+            std::string{replacement_parts[1]}
+        );
+    }
+
+    std::set<std::string> molecules{};
+    for(const auto& [from, to] : replacements)
+    {
+        std::cout << from << " => " << to << '\n';
+
+        std::size_t pos{0};
+        while(true)
+        {
+            if(auto found = medicine_molecule.find(from, pos); found != std::string::npos)
+            {
+                auto generated = std::string{medicine_molecule};
+                generated.replace(found, from.length(), to);
+                molecules.emplace(std::move(generated));
+
+                pos = found + 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    for(const auto& m : molecules)
+    {
+        std::cout << m << '\n';
+    }
+    std::cout << std::size(molecules) << '\n';
+
+    return 0;
+}
