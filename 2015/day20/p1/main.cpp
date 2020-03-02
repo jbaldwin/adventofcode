385e50168af0743cfce0b22f95b5c3898321afa7
jbaldwin <joshrbaldwin@gmail.com>
2020-03-02 13:35:39 -0700

    2015 day20 p1,p2

---
+++ 2015/day20/p1/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <lib/FileUtil.h>
+#include <lib/StringUtil.h>
+
+int main(int argc, char* argv[])
+{
+    std::vector<std::string> args{argv, argv + argc};
+    if(args.size() != 2)
+    {
+        std::cout << args[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    const auto min_presents = std::stoul(file::read(args[1]));
+    const auto max_house_num = min_presents / 10;
+
+    std::vector<uint64_t> presents{};
+    presents.resize(max_house_num);
+
+    std::cout << "Max house #" << max_house_num << "\n";
+
+    for(std::size_t i = 1; i <= max_house_num; ++i)
+    {
+        if(i % 1000 == 0)
+        {
+            std::cout << i << "\n";
+        }
+
+        auto present_per_house = i * 10;
+        for(std::size_t elf = i; elf <= max_house_num; elf += i)
+        {
+            presents[elf] += present_per_house;
+        }
+    }
+
+    uint64_t house{max_house_num};
+    for(int64_t i = max_house_num; i > 0; --i)
+    {
+        if(presents[i] >= min_presents)
+        {
+            house = i;
+        }
+    }
+
+    std::cout << "House #" << house << " has " << presents[house] << "\n";
+
+    return 0;
+}
---
+++ 2015/day20/p2/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+#include <lib/FileUtil.h>
+#include <lib/StringUtil.h>
+
+int main(int argc, char* argv[])
+{
+    std::vector<std::string> args{argv, argv + argc};
+    if(args.size() != 2)
+    {
+        std::cout << args[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    const auto min_presents = std::stol(file::read(args[1]));
+
+    std::map<std::size_t, int64_t> houses{};
+
+    std::size_t elf{1};
+    std::size_t lowest_house_num{0};
+    while(lowest_house_num == 0)
+    {
+        int64_t presents_per_house = elf * 11;
+        for(std::size_t h = 1; h <= 50; ++h)
+        {
+            auto house = h * elf;
+            houses[house] += presents_per_house;
+        }
+
+        // Its important for this problem to only check the first
+        // house if it has more than min_presents for this elf
+        // as any house past that house could still have elfs
+        // delivering presents.
+        if(houses[elf] >= min_presents)
+        {
+            lowest_house_num = elf;
+            break;
+        }
+
+        ++elf;
+    }
+
+    std::cout << "House #" << lowest_house_num << " has " << houses[lowest_house_num] << "\n";
+
+    return 0;
+}
