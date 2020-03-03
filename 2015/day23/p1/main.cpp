6ec6d46d32ee7d34780bdfa8e9f3dde0cd509b7b
jbaldwin <joshrbaldwin@gmail.com>
2020-03-02 21:24:32 -0700

    2015 day23 p1

---
+++ 2015/day23/p1/main.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <lib/FileUtil.h>
+#include <lib/StringUtil.h>
+
+struct Computer
+{
+    uint64_t a{0};
+    uint64_t b{0};
+
+    int64_t inst_ptr{0};
+    std::vector<std::string> instructions;
+
+    auto Execute() -> void
+    {
+        while(true)
+        {
+            // Program stops when an instruction outside inst.size() is used.
+            if(inst_ptr >= instructions.size() || inst_ptr < 0)
+            {
+                return;
+            }
+
+            const auto& inst = instructions[inst_ptr];
+
+            auto parts = str::split(inst, ' ');
+
+            if(parts[0] == "hlf")
+            {
+                if(parts[1] == "a")
+                {
+                    a /= 2;
+                }
+                else
+                {
+                    b /= 2;
+                }
+
+                ++inst_ptr;
+            }
+            else if(parts[0] == "tpl")
+            {
+                if(parts[1] == "a")
+                {
+                    a *= 3;
+                }
+                else
+                {
+                    b *= 3;
+                }
+
+                ++inst_ptr;
+            }
+            else if(parts[0] == "inc")
+            {
+                if(parts[1] == "a")
+                {
+                    ++a;
+                }
+                else
+                {
+                    ++b;
+                }
+
+                ++inst_ptr;
+            }
+            else if(parts[0] == "jmp")
+            {
+                inst_ptr += std::stol(std::string{parts[1]});
+            }
+            else if(parts[0] == "jie")
+            {
+                bool jump{false};
+                if(parts[1] == "a,")
+                {
+                    if(a % 2 == 0) jump = true;
+                }
+                else
+                {
+                    if(b % 2 == 0) jump = true;
+                }
+
+                if(jump)
+                {
+                    inst_ptr += std::stol(std::string{parts[2]});
+                }
+                else
+                {
+                    ++inst_ptr;
+                }
+            }
+            else if(parts[0] == "jio")
+            {
+                bool jump{false};
+                if(parts[1] == "a,")
+                {
+                    if(a == 1) jump = true;
+                }
+                else
+                {
+                    if(b == 1) jump = true;
+                }
+
+                if(jump)
+                {
+                    inst_ptr += std::stol(std::string{parts[2]});
+                }
+                else
+                {
+                    ++inst_ptr;
+                }
+            }
+            else
+            {
+                throw std::runtime_error("unknown instruction " + std::string{parts[0]});
+            }
+        }
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    std::vector<std::string> args{argv, argv + argc};
+    if(args.size() != 2)
+    {
+        std::cout << args[0] << " <input_file>" << std::endl;
+        return 0;
+    }
+
+    auto contents = file::read(args[1]);
+    auto lines = str::split(contents, '\n');
+
+    Computer comp{};
+    for(const auto& line : lines)
+    {
+        comp.instructions.push_back(std::string{line});
+    }
+
+    comp.Execute();
+
+    std::cout << "a=" << comp.a << "\n";
+    std::cout << "b=" << comp.b << "\n";
+
+    return 0;
+}
