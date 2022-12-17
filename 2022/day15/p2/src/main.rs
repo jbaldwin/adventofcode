eb696762067adbec08aac4b821db3f3c8e58507d
jbaldwin <joshrbaldwin@gmail.com>
2022-12-16 18:13:29 -0700

    2022 day15 p2

--- 2022/day15/p2/src/main.rs
+++ 2022/day15/p2/src/main.rs
@@ -1,6 +1,7 @@
 use aoc_core;
 use std::cmp::Ordering;
 use std::collections::HashSet;
+use std::process::exit;
 use std::time::Instant;
 
 #[derive(Eq, PartialEq, Hash, Clone)]
@@ -14,8 +15,8 @@ impl Point {
         Point { x, y }
     }
 
-    fn intersects(&self, p2: &Point) -> bool {
-        p2.x <= self.y
+    fn overlaps_or_touching(&self, p2: &Point) -> bool {
+        p2.x <= self.y + 1
     }
 }
 
@@ -47,7 +48,7 @@ fn main() {
     let contents = std::fs::read_to_string(&args[1]).unwrap();
     let lines = aoc_core::str_split_char(&contents, '\n');
 
-    let y = args[2].parse::<i64>().unwrap();
+    let iterations = args[2].parse::<i64>().unwrap();
 
     let mut arrangement = Vec::new();
     let mut beacons = HashSet::new();
@@ -85,83 +86,78 @@ fn main() {
     let elapsed = now.elapsed();
     println!("setup {}ms", elapsed.as_millis());
 
-    let now = Instant::now();
-    // Prepopulate beacons as 'searched' but don't count towards the total.
-    let mut beacons_in_row = 0i64;
-    for beacon in beacons.iter() {
-        if beacon.y == y {
-            // y_row[(beacon.x + min_x_abs) as usize] = true;
-            beacons_in_row += 1;
-        }
-    }
-
     let mut ranges = Vec::with_capacity(arrangement.len());
 
-    for sensor in arrangement.iter() {
-        let s = &sensor.s;
-        let y_dist = (s.y - y).abs();
+    let now = Instant::now();
+    for y in 0..=iterations {
+        ranges.clear();
 
-        // This sensor can never reach the Y row.
-        if y_dist > sensor.md {
-            continue;
+        for sensor in arrangement.iter() {
+            let s = &sensor.s;
+            let y_dist = (s.y - y).abs();
+    
+            // This sensor can never reach the Y row.
+            if y_dist > sensor.md {
+                continue;
+            }
+    
+            let x_remaining_dist = sensor.md - y_dist;
+            ranges.push(Point::new(s.x - x_remaining_dist, s.x + x_remaining_dist));
         }
 
-        let x_remaining_dist = sensor.md - y_dist;
-        ranges.push(Point::new(s.x - x_remaining_dist, s.x + x_remaining_dist));
-    }
-
-    // merge ranges
-    ranges.sort_by(|p1, p2| {
-        if p1.x < p2.x {
-            Ordering::Less
-        }
-        else if p1.x == p2.x {
-            if p1.y == p2.y {
-                Ordering::Equal
-            } else if p1.y < p2.y {
+        // merge ranges
+        ranges.sort_by(|p1, p2| {
+            if p1.x < p2.x {
                 Ordering::Less
+            }
+            else if p1.x == p2.x {
+                if p1.y == p2.y {
+                    Ordering::Equal
+                } else if p1.y < p2.y {
+                    Ordering::Less
+                } else {
+                    Ordering::Greater
+                }
             } else {
                 Ordering::Greater
             }
-        } else {
-            Ordering::Greater
-        }
-    });
+        });
 
-    let mut i = 0usize;
-    while i < ranges.len() - 1 {
-        let mut combine = false;
-        let mut new_y = 0i64;
-        {
-            let p1 = &ranges[i];
-            let p2 = &ranges[i + 1];
+        let mut i = 0usize;
+        while i < ranges.len() - 1 {
+            let mut combine = false;
+            let mut new_y = 0i64;
+            {
+                let p1 = &ranges[i];
+                let p2 = &ranges[i + 1];
 
-            // combine if they intersect and it would extend it
-            if p1.intersects(p2) {
-                combine = true;
-                new_y = std::cmp::max(p1.y, p2.y);
+                // combine if they intersect and it would extend it
+                if p1.overlaps_or_touching(p2) {
+                    combine = true;
+                    new_y = std::cmp::max(p1.y, p2.y);
+                }
+            }
+
+            if combine {
+                ranges.get_mut(i).unwrap().y = new_y;
+                ranges.remove(i + 1);
+            } else {
+                i += 1;
             }
         }
 
-        if combine {
-            ranges.get_mut(i).unwrap().y = new_y;
-            ranges.remove(i + 1);
-        } else {
-            i += 1;
+        if ranges.len() > 1 {
+            for range in ranges.iter() {
+                print!("[{}]", range);
+            }
+            let distress_beacon = Point::new(ranges.first().unwrap().y + 1, y);
+
+            println!("\n[{}]", distress_beacon);
+            println!("{}", distress_beacon.x * 4000000 + y);
+
+            let elapsed = now.elapsed();
+            println!("\n{}ms", (elapsed.as_micros() as f64) / 1000f64);
+            exit(0);
         }
     }
-
-    println!();
-    let mut total = -beacons_in_row;
-    for range in ranges.iter() {
-        println!("[{}], amount={}", range, range.y.abs() + range.x.abs());
-        total += range.y.abs() + range.x.abs();
-        if range.intersects(&Point::new(0, 0)) {
-            total += 1;
-        }
-    }
-
-    let elapsed = now.elapsed();
-
-    println!("{} in {}ms", total, (elapsed.as_micros() as f64) / 1000f64);
 }
